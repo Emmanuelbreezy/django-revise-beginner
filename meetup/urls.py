1ffd842d2bdb3b600713f37ea71442a9278f2b0b
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2021-12-13 13:43:27 +0100

    first commit

--- meetup/urls.py
+++ meetup/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('meetup/', views.index, name='all-meetups'),
+    path('meetup/success', views.confirm_registration, name='confirm-register'),
     path('meetup/<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
 ]
